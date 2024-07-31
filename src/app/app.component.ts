298f749a62b3e0d0f90e94be7a9c22edae02f8c3
Sahil-Mishra63 <sahilmishrayt63@gmail.com>
2024-08-01 00:48:52 +0530

    Added the header/navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./MyComponents/header/header.component";
+import { FooterComponent } from "./MyComponents/footer/footer.component";
+import { MainPageComponent } from "./MyComponent/main-page/main-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
